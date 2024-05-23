d76d93f102a33c6ad65f52f0b6507886d2400c3b
Aravinth_Siva <68836256+AravinthS75@users.noreply.github.com>
2024-05-23 14:28:56 +0530

    Add files via upload

---
+++ Healthy Product/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.css"
+function Footer(){
+
+    return<>
+    {
+        
+        <footer className={styles.footer}>
+        <p>&copy; 2024 Aravinth Corp. All rights reserved.</p>
+        </footer>
+    }
+    </>
+}   
+
+export default Footer
