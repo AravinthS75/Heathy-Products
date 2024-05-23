c1c0ad0ee3b363b9b9c7cd848a203f7ed4547487
Aravinth_Siva <68836256+AravinthS75@users.noreply.github.com>
2024-05-23 14:58:38 +0530

    Add files via upload

---
+++ Healthy Product/src/pages/nopage/Nopage.jsx
@@ -0,0 +1,13 @@
+import styles from "./Nopage.module.css";
+
+function Nopage(){
+
+    return<>
+    <div className={styles.contentText}>
+    <h1>404 Error</h1>
+    <h3>Page Not Found!</h3>
+    </div>
+    </>
+}
+
+export default Nopage
