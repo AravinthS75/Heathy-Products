abc250e42c134eae5c9b25ddcf90eedc8ec14dda
Aravinth_Siva <68836256+AravinthS75@users.noreply.github.com>
2024-05-23 14:25:14 +0530

    Add files via upload

---
+++ Healthy Product/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import styles from "./Header.module.css";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
+function Header({ loginStatus, setLoginStatus }) {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        setLoginStatus(false);
+        localStorage.removeItem('loginStatus', false);
+        localStorage.removeItem('Token');
+        localStorage.removeItem('order');
+    };
+
+    const handleClick = (e) => {
+        if (!loginStatus) {
+            e.preventDefault();
+            navigate('/login');
+        }
+    };
+
+    return (
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <Link to="/home" className={styles.navLink} onClick={handleClick}>
+                    Home
+                </Link>
+                <Link to="/about" className={styles.navLink} onClick={handleClick}>
+                    About
+                </Link>
+                <Link to="/contact" className={styles.navLink} onClick={handleClick}>
+                    Contact
+                </Link>
+                <Link to="/blogs" className={styles.navLink} onClick={handleClick}>
+                    Blogs
+                </Link>
+                <Link to="/store" className={styles.navLink} onClick={handleClick}>
+                    Store
+                </Link>
+            </nav>
+            <Link to="/" className={!loginStatus ? styles.logoA : styles.logoB}>Helthy Products</Link>
+            <div className={styles.auth}>
+                {!loginStatus ? (
+                    <>
+                        <Link to="/login" className={styles.login}>Login</Link>
+                        <Link to="/signup" className={styles.signup}>SignUp</Link>
+                    </>
+                ) : (
+                    <>
+                    <Link to="/" className={styles.logout} onClick={handleLogout}>Logout</Link>
+                    <Link to="/myorder" className={styles.myorder}>MyOrder</Link>
+                    </>
+                )}
+            </div>
+            <Outlet />
+        </header>
+    );
+}
+
+export default Header;
